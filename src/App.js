41793252cde677bedad7cc70654d21ec70020f95
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-19 14:10:14 +0300

    adding booklist component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import BookForm from './components/BookForm';
+import BookList from './components/BookList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <BookList />
+      <BookForm />
     </div>
   );
 }
---
+++ src/components/BookForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BookForm() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default BookForm
