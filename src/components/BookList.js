1635b928ce69d53a3f80884b647d5d03c183bd9e
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-19 14:29:01 +0300

    creating the table for book

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,7 +3,23 @@ import React from 'react'
 function BookList() {
     return (
         <div>
-            
+            <table>
+                <thead>
+                    <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Category</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>id</td>
+                        <td>title</td>
+                        <td>Category</td>
+                    </tr>
+                </tbody>
+
+            </table>
         </div>
     )
 }
