38419b4044eae1a47ee1b254e3a99d849c261968
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-20 22:07:25 +0300

    add combine reducer

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Book from '../components/Book';
+import { useDispatch } from 'react-redux';
 
 const BookList = () => {
   const books = useSelector((state) => state);
   const bookList = books.map((book) => <Book key={book.id} book={book} />);
-
+  const dispatch = useDispatch()
   return (
     <div className="book-container">
       <table>
