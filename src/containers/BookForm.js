319c09457e89712eb725f80369984aa86f180040
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-20 21:04:45 +0300

    adding key to BookForm

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function BookForm() {
   const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  const category = bookCategories.map((element) => <option value="">{element}</option>);
+  const category = bookCategories.map((item) => <option value="" key={item}>{item}</option>);
   return (
     <div>
       <form action="add-book">
