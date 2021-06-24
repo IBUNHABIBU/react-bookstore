25f74e7fa2a4e3850ffbbb7748d0892517585a56
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-24 15:18:05 +0300

    removing the lint errors

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,32 +1,31 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 function BookForm() {
   const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   const category = bookCategories.map((item) => <option value="" key={item}>{item}</option>);
   const [title, setTitle] = useState('');
-  const [cat, setCat] = useState(initialState);
-  const book = useSelector((state) => state);
+  const [cat, setCat] = useState('');
   const dispatch = useDispatch();
   const handleChange = (e) => {
-      setTitle(e.target.value);
-      setCat(e.target.value);
-  }
+    setTitle(e.target.value);
+    setCat(e.target.value);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(handleChange())
-  }
+    dispatch(handleChange());
+  };
   return (
     <div>
-      <form action="add-book" onSubmit = {handleSubmit}>
+      <form action="add-book" onSubmit={handleSubmit}>
         <label htmlFor="Title">
           Title
-          <input type="text" value ={title} onChange={handleChange}/>
+          <input type="text" value={title} onChange={handleChange} />
         </label>
-        <select name="cat" id="cat" onChange = {handleChange}>
+        <select name="cat" id="cat" value={cat} onChange={handleChange}>
           {category}
         </select>
-        <button type="submit" >Add Book</button>
+        <button type="submit">Add Book</button>
       </form>
     </div>
   );
