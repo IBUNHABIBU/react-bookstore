abfd5c10f5cda78577ce6b46cb7b1cf467d7d02d
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-24 15:00:26 +0300

    add handleChange and handleSubmit methods

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 function BookForm() {
   const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   const category = bookCategories.map((item) => <option value="" key={item}>{item}</option>);
+  const [title, setTitle] = useState('');
   const book = useSelector((state) => state);
   const dispatch = useDispatch();
+  const handleChange = (e) => {
+      setTitle(e.target.value);
+  }
   return (
     <div>
-      <form action="add-book">
+      <form action="add-book" onSubmit = {handleSubmit}>
         <label htmlFor="Title">
           Title
-          <input type="text" />
+          <input type="text" value ={title} onChange={handleChange}/>
         </label>
-        <select name="cat" id="cat">
+        <select name="cat" id="cat" onChange = {handleChange}>
           {category}
         </select>
-        <button type="submit" onClick={() => dispatch(book)}>Add Book</button>
+        <button type="submit" >Add Book</button>
       </form>
     </div>
   );
