d03504085e0ea1373d12ba2f1f4e42c4285b6c73
IBUNHABIBU <ibunhabib@gmail.com>
2021-06-29 18:03:37 +0300

    Fix the lint errors

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,16 +1,15 @@
 import React from 'react';
-const cat = ['All','Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-const CategoryFilter = () => {
-    return (
-        <div>
-            <label htmlFor="Category">
-          Category
-          <select name="cat" id="cat" value={book.category} onChange={handleCategoryChange}>
-            {cat}
-          </select>
-        </label>
-        </div>
-    )
-}
+
+const cat = ['All', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+const CategoryFilter = () => (
+  <div>
+    <label htmlFor="Category">
+      Category
+      <select name="cat" id="cat" value={book.category} onChange={handleCategoryChange}>
+        {cat}
+      </select>
+    </label>
+  </div>
+);
 
 export default CategoryFilter;
--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,16 +1,17 @@
 import { CHANGE_FILTER } from '../actions/constants';
+
 const cat = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 const initialState = {
-    filter: "All"
-}
+  filter: 'All',
+};
 
 const filterReducer = (state, action) => {
   switch (action.type) {
-      case CHANGE_FILTER:
-          
-          break;
-  
-      default:
-          break;
+    case CHANGE_FILTER:
+
+      break;
+
+    default:
+      break;
   }
-}
+};
