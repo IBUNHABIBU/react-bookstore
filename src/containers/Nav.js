7764fa1a5ba2d7a0c2121fc4b433cd8ce91cb845
IBUNHABIBU <ibunhabib@gmail.com>
2021-07-05 17:33:16 +0300

    Add filter to the navbar

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import {
   AppBar, Toolbar, Typography,
 } from '@material-ui/core';
 import myStyle from '../stylesheet/style';
+import CategoryFilter from '../components/CategoryFilter';
+import { changeFilter } from '../actions/index';
 
 const Nav = () => {
   const classes = myStyle();
+  const dispatch = useDispatch();
+  const handleCategoryChange = (e) => {
+    dispatch(changeFilter(e.target.value));
+  };
 
   return (
     <AppBar position="static" color="transparent">
@@ -17,9 +24,7 @@ const Nav = () => {
         <Typography variant="subtitle1" className={classes.title} color="primary">
           BookStore CMS
         </Typography>
-        <Typography variant="subtitle2" className={classes.title} color="primary">
-          BookStore CMS
-        </Typography>
+        <CategoryFilter handleFilter={handleCategoryChange} />
       </Toolbar>
     </AppBar>
   );
