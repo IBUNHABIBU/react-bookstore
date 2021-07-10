a3cfe0545518136ba9dee93da389b0a86c9a9658
IBUNHABIBU <ibunhabib@gmail.com>
2021-07-10 15:34:48 +0300

    Style the booklist

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,9 +5,20 @@ function Book({ book, handleRemoveBook }) {
   const { title, category } = book;
   return (
     <div className="book-container">
-      <p>{category}</p>
-      <h2>{title}</h2>
-      <button type="submit" onClick={() => handleRemoveBook(book)}>Remove Book</button>
+      <div>
+        <p>{category}</p>
+        <h2>{title}</h2>
+        <button type="submit" onClick={() => handleRemoveBook(book)}>Remove Book</button>
+      </div>
+      <div className="status-bar">
+        <p>64%</p>
+        <p>Completed</p>
+      </div>
+      <div className="chapter">
+        <p>current chapter</p>
+        <p>Chapter 17</p>
+        <button type="submit">Update progress</button>
+      </div>
     </div>
   );
 }
