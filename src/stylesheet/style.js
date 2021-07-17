a58d8e3f9bcca2d18e2f059c024d08cc94359cdf
IBUNHABIBU <ibunhabib@gmail.com>
2021-07-17 20:53:58 +0300

    Style the booklist page

--- src/stylesheet/style.js
+++ src/stylesheet/style.js
@@ -1,14 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const myStyle = makeStyles(() => ({
-  root: {
-  //   width: '67.29rem',
-  //   height: '5.938rem',
-  //   margin: '0 0 2.375rem',
-  //   padding: '0.001rem 6.188rem 1.688rem 0.001rem',
-  //   // backgroundColor: 'var(--white)',
-  //   backgroundColor: 'red',
-  },
   toolbar: {
     width: '73.3rem',
     height: '5.938rem',
