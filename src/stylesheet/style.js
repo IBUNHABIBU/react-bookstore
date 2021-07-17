6da721b04a041127f03f634486acf0f98a5d98be
IBUNHABIBU <ibunhabib@gmail.com>
2021-07-17 18:44:40 +0300

    Style the bookstore title in the nav

--- src/stylesheet/style.js
+++ src/stylesheet/style.js
@@ -6,8 +6,20 @@ const myStyle = makeStyles(() => ({
     height: '5.938rem',
     margin: '0 0 2.375rem',
     padding: '0.001rem 6.188rem 1.688rem 0.001rem',
-    // backgroundColor: 'var(--white)',
-    backgroundColor: 'red',
+    backgroundColor: 'var(--white)',
+  },
+  logo: {
+    width: '15rem',
+    height: '2.313rem',
+    margin: '0.375rem 2.938rem 0.125rem 0',
+    fontFamily: 'Montserrat, Sans-serif',
+    fontSize: '1.875rem',
+    fontWeight: 'bold',
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+    lineHeight: 'normal',
+    letterSpacing: 'normal',
+    color: 'var(--azure)',
   },
 }));
 
