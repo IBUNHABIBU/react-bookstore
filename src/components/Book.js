659a88958d5dca00251f6b2f04c4f1afeffed21c
IBUNHABIBU <ibunhabib@gmail.com>
2021-07-18 23:24:49 +0300

    Style the divider

--- src/components/Book.js
+++ src/components/Book.js
@@ -30,7 +30,7 @@ function Book({ book, handleRemoveBook }) {
           <p className="completed">Completed</p>
         </div>
       </div>
-      <Divider orientation="vertical" />
+      <span className="horizontal-line"/>
       <div className="chapter">
         <p className="current">current chapter</p>
         <p className="chapter-number">Chapter 17</p>
